be009a526e8c2db6449543a23ce71d3ba7e4be1e
Jose David <jose7312000@gmail.com>
2022-10-05 19:23:41 -0500

    Optimizando codigo

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -5,25 +5,31 @@ const router = express.Router()
 const response = require('../../../network/response');
 const Controller = require('./index')
 
-router.get('/', function(req, res){
-   Controller.list()
-      .then((lista) => {
-        response.success(req, res, lista, 200)
-      })
-      .catch((err) =>{
-        response.success(req, res, lista, 500)
-      })
-});
+//Routes
+router.get('/', list);
+router.get('/:id', get);
 
-router.get('/:id', function(req, res){
+
+//Funciones
+function list(req, res) {
+    Controller.list()
+        .then((lista) => {
+            response.success(req, res, lista, 200)
+        })
+        .catch((err) => {
+            response.success(req, res, lista, 500)
+        })
+}
+
+
+function get(req, res) {
     Controller.get(req.params.id)
-       .then((user) => {
-         response.success(req, res, user, 200)
-       })
-       .catch((err) =>{
-         response.success(req, res, user, 500)
-       })
- });
- 
+        .then((user) => {
+            response.success(req, res, user, 200)
+        })
+        .catch((err) => {
+            response.success(req, res, user, 500)
+        })
+}
 
 module.exports = router
--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const Controller = require('./components/user/network')
+const Controller = require('./components/user/network');
 
 const port = 3000;
+app.use(bodyParser.json());
 app.use(express.json())//Metodo para analizar las solicitudes entrantes
 
 app.use('/api/user', Controller)
